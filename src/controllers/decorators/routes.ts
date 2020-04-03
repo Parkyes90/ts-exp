7d15f8fc23b0aadf2baabba9b4243d2443964274
parkyes90 <parkyes90@gmail.com>
2020-04-03 16:35:03 +0900

    feat(ProjectFileDir): Add typing

--- src/controllers/decorators/routes.ts
+++ src/controllers/decorators/routes.ts
@@ -1,9 +1,14 @@
 import "reflect-metadata";
 import { Methods } from "./Methods";
+import { RequestHandler } from "express";
+
+interface RouteHandlerDescriptor extends PropertyDescriptor {
+  value?: RequestHandler;
+}
 
 export function routeBinder(method: Methods) {
   return function (path: string) {
-    return function (target: any, key: string, desc: PropertyDescriptor) {
+    return function (target: any, key: string, desc: RouteHandlerDescriptor) {
       Reflect.defineMetadata("path", path, target, key);
       Reflect.defineMetadata("method", method, target, key);
     };
