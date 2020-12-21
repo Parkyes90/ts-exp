722fd17817910ee4acd680ecf1bcb3974eb41c56
parkyes90 <parkyes90@gmail.com>
2020-12-21 15:34:40 +0900

    add deps

---
+++ config/settings.py
@@ -0,0 +1,7 @@
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+DATA_DIR = os.path.join(BASE_DIR, "data")
+OUTPUTS_DIR = os.path.join(DATA_DIR, "outputs")
+INPUTS_DIR = os.path.join(DATA_DIR, "inputs")
