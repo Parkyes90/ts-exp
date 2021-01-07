13ef82752bad0b96fa57638a45faf42c53eb5261
ParkYeseung <parkyes90@gmail.com>
2021-01-07 17:56:27 +0900

    add H_IN_DIRS in config

--- config/settings.py
+++ config/settings.py
@@ -6,3 +6,4 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_DIR = os.path.join(BASE_DIR, "data")
 OUTPUTS_DIR = os.path.join(DATA_DIR, "outputs")
 INPUTS_DIR = os.path.join(DATA_DIR, "inputs")
+H_IN_DIRS = os.path.join(INPUTS_DIR, "happiness")
